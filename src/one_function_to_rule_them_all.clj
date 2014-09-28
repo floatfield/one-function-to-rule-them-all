ed50dc31b6ebf6ff3aa94eb7a6f99da929d144e7
floatfield <floatfield@gmail.com>
2014-09-28 13:54:30 +0400

    temporary-result

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,22 +1,31 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce #(str %1 " " %2) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '()
+    (seq (reduce #(conj %1 x %2) [(first a-seq)] (rest a-seq)))))
 
 (defn my-count [a-seq]
-  :-)
+  (let [helper (fn [counter elem]
+                 (inc counter))]
+    (reduce helper 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (let [helper (fn [new-seq elem]
+                 (conj new-seq elem))]
+    (reduce helper '() a-seq)))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (let [helper (fn [res elem])
+        ]))
 
 (defn insert [sorted-seq n]
   [:-])
